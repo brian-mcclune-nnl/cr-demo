440260eb1cd00c8e81849c65852e8444d0f2fe47
Brian McClune <bpmcclune@gmail.com>
2023-02-23 16:45:52 +0000

    Capture restore logs to file

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ PID_FILE="$CHECKPOINT_DIR/$(date +%Y%m%d%H%M%S%N).pid"
 if [[ -e $CHECKPOINT_DIR ]] && [[ ! -z "$(ls $CHECKPOINT_DIR | grep fib)" ]]; then
     LAST_CHECKPOINT_DIR=$CHECKPOINT_DIR/$(ls -tr $CHECKPOINT_DIR | grep fib | tail -1)
     sudo criu restore -vvvv --shell-job -D $LAST_CHECKPOINT_DIR \
+	-o restore.log \
 	--pidfile $PID_FILE
 else
     /usr/bin/python "$(realpath $(dirname $0))/fib.py" 100 --sleepy &
